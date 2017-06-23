bdf5c482e06e6bb4a6fdcd40211cada11a809749
Rafael Chuque <Rafa_chuque@hotmail.com>
2017-06-23 15:18:09 -0300

    Tutorial SQS Standard e FIFO v2

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -17,24 +17,6 @@
  * permissions and limitations under the License.
  */
 
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.Scanner;
-
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClient;
-import com.amazonaws.services.sqs.model.CreateQueueRequest;
-import com.amazonaws.services.sqs.model.DeleteMessageRequest;
-import com.amazonaws.services.sqs.model.DeleteQueueRequest;
-import com.amazonaws.services.sqs.model.Message;
-import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
-import com.amazonaws.services.sqs.model.SendMessageRequest;
 
 /**
  * This sample demonstrates how to make basic requests to Amazon SQS using the
@@ -53,96 +35,9 @@ import com.amazonaws.services.sqs.model.SendMessageRequest;
  */
 public class MainClass {
     public static void main(String[] args) {
-        /*
-         * The ProfileCredentialsProvider returns your [default]
-         * credential profile by reading from the credentials file located at
-         * (~/.aws/credentials).
-         */
-        AWSCredentials credentials = null;
-        try {
-            credentials = new ProfileCredentialsProvider().getCredentials();
-        } catch (Exception e) {
-            throw new AmazonClientException(
-                    "Cannot load the credentials from the credential profiles file. " +
-                            "Please make sure that your credentials file is at the correct " +
-                            "location (~/.aws/credentials), and is in valid format.",
-                    e);
-        }
 
-        AmazonSQS sqs = new AmazonSQSClient(credentials);
-        Region saEast = Region.getRegion(Regions.SA_EAST_1);
-        sqs.setRegion(saEast);
+        new StandardQueue().startProcess();
+        new FIFOQueue().startProcess();
 
-        System.out.println("===========================================");
-        System.out.println("Getting Started with Amazon SQS");
-        System.out.println("===========================================\n");
-
-        Scanner scanner = new Scanner(System.in);
-
-        try {
-            // Create a queue
-            System.out.println("Creating a new SQS queue called MyQueue.\n");
-            CreateQueueRequest createQueueRequest = new CreateQueueRequest("MyQueue");
-            String myQueueUrl = sqs.createQueue(createQueueRequest).getQueueUrl();
-
-            scanner.nextLine();
-            // List queues
-            System.out.println("Listing all queues in your account.\n");
-            for (String queueUrl : sqs.listQueues().getQueueUrls()) {
-                System.out.println("  QueueUrl: " + queueUrl);
-            }
-            System.out.println();
-            scanner.nextLine();
-
-            // Send a message
-            System.out.println("Sending a message to MyQueue.\n");
-            sqs.sendMessage(new SendMessageRequest(myQueueUrl, "This is my message text."));
-            scanner.nextLine();
-
-            // Receive messages
-            System.out.println("Receiving messages from MyQueue.\n");
-            ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(myQueueUrl);
-            List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
-            for (Message message : messages) {
-                System.out.println("  Message");
-                System.out.println("    MessageId:     " + message.getMessageId());
-                System.out.println("    ReceiptHandle: " + message.getReceiptHandle());
-                System.out.println("    MD5OfBody:     " + message.getMD5OfBody());
-                System.out.println("    Body:          " + message.getBody());
-                for (Entry<String, String> entry : message.getAttributes().entrySet()) {
-                    System.out.println("  Attribute");
-                    System.out.println("    Name:  " + entry.getKey());
-                    System.out.println("    Value: " + entry.getValue());
-                }
-            }
-
-
-            System.out.println();
-            scanner.nextLine();
-
-
-            // Delete a message
-            System.out.println("Deleting a message.\n");
-            String messageReceiptHandle = messages.get(0).getReceiptHandle();
-            sqs.deleteMessage(new DeleteMessageRequest(myQueueUrl, messageReceiptHandle));
-            scanner.nextLine();
-
-            // Delete a queue
-            System.out.println("Deleting the test queue.\n");
-            sqs.deleteQueue(new DeleteQueueRequest(myQueueUrl));
-        } catch (AmazonServiceException ase) {
-            System.out.println("Caught an AmazonServiceException, which means your request made it " +
-                    "to Amazon SQS, but was rejected with an error response for some reason.");
-            System.out.println("Error Message:    " + ase.getMessage());
-            System.out.println("HTTP Status Code: " + ase.getStatusCode());
-            System.out.println("AWS Error Code:   " + ase.getErrorCode());
-            System.out.println("Error Type:       " + ase.getErrorType());
-            System.out.println("Request ID:       " + ase.getRequestId());
-        } catch (AmazonClientException ace) {
-            System.out.println("Caught an AmazonClientException, which means the client encountered " +
-                    "a serious internal problem while trying to communicate with SQS, such as not " +
-                    "being able to access the network.");
-            System.out.println("Error Message: " + ace.getMessage());
-        }
     }
 }
---
+++ src/main/java/SQSQueue.java
@@ -0,0 +1,5 @@
+/**
+ * Created by rafa_ on 23/06/2017.
+ */
+public abstract class SQSQueue {
+}
